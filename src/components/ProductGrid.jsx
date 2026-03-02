e1aac7f0e986e12a6bf025d6d88e9f882a794141
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-03-02 18:47:15 +0100

    Refactor ProductGrid component to use static product data instead of fetching from JSON

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,27 +1,10 @@
-import { useState, useEffect } from "react";
 import Product from "./Product";
+import productsData from "../products.json";
 
 export default function ProductGrid() {
-  const [productsState, setProducts] = useState([]);
-
-  useEffect(() => {
-    console.log("useEffect starter");
-
-    fetch("/products.json")
-      .then((res) => {
-        console.log("Response:", res.status, res.ok);
-        return res.json();
-      })
-      .then((data) => {
-        console.log("Data modtaget:", data);
-        setProducts(data);
-      })
-      .catch((err) => console.error("Fejl:", err));
-  }, []);
-
   return (
     <section className="product-grid">
-      {productsState.map((product) => (
+      {productsData.map((product) => (
         <Product key={product.id} product={product} />
       ))}
     </section>
