44d719abdbb86c1fd932ab279636a0b557802192
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-03-02 09:44:14 +0100

    Fix typo in welcome message on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
         <h1>Home</h1>
       </header>
       <main>
-        <p>Welcome to the home page...</p>
+        <p>Welcome to the home page.</p>
 
         <article>
           <h2>Displaying images in React</h2>
