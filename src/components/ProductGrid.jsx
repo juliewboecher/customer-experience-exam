0905f5ea72756c27450ca61f38cbd73214cc97cc
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-03-02 17:28:28 +0100

    Add ProductGrid component for fetching and displaying products

---
+++ src/components/ProductGrid.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import Product from "./Product";
+
+export default function ProductGrid() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const url =
+        "https://raw.githubusercontent.com/cederdorff/race/refs/heads/master/data/webshop/products.json";
+      const response = await fetch(url);
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    }
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <section className="product-grid">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </section>
+  );
+}
