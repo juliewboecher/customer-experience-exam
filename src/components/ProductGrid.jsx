f1d5c0394be8044b541a57eed4970581175434f8
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-03-03 17:56:56 +0100

    Fix fetch URL in ProductGrid component to use relative path for products.json

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -7,7 +7,7 @@ export default function ProductGrid() {
   useEffect(() => {
     async function fetchProducts() {
       const url =
-        "/products.json";
+        "products.json";
       const response = await fetch(url);
       console.log(response);
       const data = await response.json();
