bc00dbdb993ce3baf1b7669d71387797d55331c8
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-03-03 18:03:43 +0100

    Update Navbar links to reflect localized content and improve user experience

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router";
 export default function Navbar() {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/products">Products</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-      <NavLink to="/services">Services</NavLink>
+      <NavLink to="/">billede af logo</NavLink>
+      <NavLink to="/products">Produkter</NavLink>
+      <NavLink to="/about">Mærker</NavLink>
+      <NavLink to="/contact">Kategorier</NavLink>
+      <NavLink to="/services">Nyheder</NavLink>
     </nav>
   );
 }
