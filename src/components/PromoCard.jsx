a0c99b7ce34f1b135b7839197c89ea8a426ceca4
cecilieskals <eaa25cts@students.eaaa.dk>
2026-03-05 11:26:07 +0100

    opbygning af promo cards

---
+++ src/components/PromoCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+
+export default function PromoCard ({ promo }) {
+  if (!promo) return null;
+  
+  return (
+    <article className="promo-card">
+      <section className="promo-card-info">
+        <h2 className="promo-title">{promo.title}</h2>
+        <p className="promo-description">{promo.description}</p>
+      </section>
+
+      <img src={promo.image} className="promo-card-image" alt={promo.title} />
+
+      <footer className="promo-card-meta">
+        <button className="promo-card-button">
+          <p className="promo-card-button-text">Shop Nu</p>
+        </button>
+      </footer>
+    </article>
+  );
+}
