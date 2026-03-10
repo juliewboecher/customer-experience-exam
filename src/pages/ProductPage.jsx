e675062efb8ca0f7454f6cd32d7d97176f860ef9
FKAYSR <kejserfreja@gmail.com>
2026-03-10 13:38:34 +0100

    Add FilterBar component to ProductPage for enhanced filtering options

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,20 @@
 import ProductGrid from "../components/ProductGrid";
 import CategoryCarrusel from "../components/CategoryCarrusel";
 import { useSearchParams } from "react-router";
+import FilterBar from "../components/FilterBar";
 
 export default function ProductPage() {
   const [searchParams] = useSearchParams();
   const categoryFromUrl = searchParams.get("category") || "Alle";
   const searchQuery = searchParams.get("search") || "";
   const showCarousel = categoryFromUrl === "Alle" && !searchQuery;
+  const color = searchParams.get("color") || "";
+  const size = searchParams.get("size") || "";
+  const brand = searchParams.get("brand") || "";
+  const prize = searchParams.get("prize") || "";
+  const condition = searchParams.get("condition") || "";
+  const inStock = searchParams.get("inStock") === "true";
+  const sort = searchParams.get("sort") || "";
 
   return (
     <>
@@ -29,9 +37,17 @@ export default function ProductPage() {
           />
         )}
       </section>
+      <FilterBar />
       <ProductGrid
         selectedCategory={categoryFromUrl}
         searchQuery={searchQuery}
+        color={color}
+        size={size}
+        brand={brand}
+        prize={prize}
+        condition={condition}
+        inStock={inStock}
+        sort={sort}
       />
     </>
   );
